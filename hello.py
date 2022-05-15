575dc122deef5d4a0bbb1e2f1eda2245add031e6
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-15 20:11:45 +0300

    build: installed all dependencies

---
+++ hello.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+
+# Create a flask instance
+
+app = Flask(__name__)
+
+
+# Create a route decorator
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template('user.html', name=name)
+
+# Custom error page
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
