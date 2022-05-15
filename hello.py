d53ac556f939ad25cace6db05c1e1557499bd091
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-15 23:06:01 +0300

    added navbar content

--- hello.py
+++ hello.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+# Users page
 
 @app.route("/user/<name>")
 def user(name):
     return render_template('user.html', name=name)
 
-# Custom error page
+
+# Custom errors page
 
 @app.errorhandler(404)
 def page_not_found(e):
