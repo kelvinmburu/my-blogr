4c0991a0d29c96379ea20a1a634d126cd60f88f7
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-16 23:27:40 +0300

    chore: updated all files

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'acd6b60c667a94557bce0aec3c409354'
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgress:kevin@localhost/myblogr'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TSL = True
--- manage.py
+++ manage.py
@@ -1,8 +1,20 @@
+from flask_script import Manager, Server
 from flaskblog import create_app
+from flask_migrate import Migrate, MigrateCommand
+from flaskblog.models import User, Post
 
 app = create_app('production')
 
+manager = Manager(app)
+manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@Manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+
     
