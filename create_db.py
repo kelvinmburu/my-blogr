d04999512b63f854cda3c751f2ed237aec03f3c3
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-16 23:00:26 +0300

    installed all dependencies from requirements

---
+++ create_db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+#mydb = mysql.connector.connect(host="localhost",user="moringa", password="passworD@23", auth_plugin='mysql_native_password')
+
+# my_cursor = mydb.cursor()
+
+# my_cursor.execute("CREATE DATABASE users")
+
+# my_cursor.execute("SHOW DATABASES")
+# for db in my_cursor:
+#     print(db)
