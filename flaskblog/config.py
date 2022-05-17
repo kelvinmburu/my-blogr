0de078ef124c6b1ccb67abdf5ebd2b8cda8bc1df
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-17 10:02:59 +0300

    build: installed migrations and transfered data

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'acd6b60c667a94557bce0aec3c409354'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgress:kevin@localhost/myblogr'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:mburu@localhost/myblogr'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TSL = True
@@ -19,11 +19,11 @@ class ProdConfig(Config):
     DEBUG = True
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:mburu@localhost/myblogr'
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:mburu@localhost/myblogr'
 
 
 config_options = {
